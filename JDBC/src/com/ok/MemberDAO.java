035fdc9a3574f576ef066aaea672a8f9f548e711
loveen95 <leeyounbin@naver.com>
2022-02-04 17:57:16 +0900

    20220204

--- JDBC/src/com/ok/MemberDAO.java
+++ JDBC/src/com/ok/MemberDAO.java
@@ -6,6 +6,12 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import javax.naming.*;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+
+
 public class MemberDAO { 
 	
 	/*
@@ -14,6 +20,10 @@ public class MemberDAO {
 	 * 싱글톤 패턴을 적용하여 객체를 1개로 제한 
 	 * 
 	 * */
+	private DataSource ds;  //데이터 소스 객체 생성
+	private Context ct;
+	
+	
 	//1. 스스로 객체를 멤버변수로 선언하고, 1개로 제한
 	private static MemberDAO instance = new MemberDAO();
 	
@@ -21,9 +31,10 @@ public class MemberDAO {
 	private MemberDAO() {
 		//생성자가 한번 동작 할때에 다음 내용을 처리....
 		try {
-			Class.forName("oracle.jdbc.driver.OracleDriver");
+			ct = new InitialContext(); //이니셜 컨텍스트 객체 생성
+			ds = (DataSource)ct.lookup("java:comp/env/jdbc/oracle"); // 이니셜 컨텍스트로 부터 찾음/
 		}catch (Exception e) {
-			System.out.println("드라이버 호출시 에러 발생");
+			e.printStackTrace();
 		}
 	}
 	// 3. 외부에서 객체를 요구할때 getter 매서드만 써서 반환
@@ -32,10 +43,10 @@ public class MemberDAO {
 	}
 	//DB연동을 위한 필요한 변수와 객체를 선언
 	
-	private String url = "jdbc:oracle:thin:@localhost:1521/XEPDB1";
-	private String user ="myjsp";
-	private String password = "myjsp";
-	
+//	private String url = "jdbc:oracle:thin:@localhost:1521/XEPDB1";
+//	private String user ="myjsp";
+//	private String password = "myjsp";
+//	
 	private Connection conn = null;        //모든 값을 은닉해야함.
 	private PreparedStatement pstmt = null;
 	private ResultSet rs = null;  //ResultSet 객체도 생성 해야함.
@@ -48,7 +59,9 @@ public class MemberDAO {
 		String sql = "insert into testusers values(?,?,?,?,?,?,?)";
 		try {
 			//Connection 객체 생성
-			conn = DriverManager.getConnection(url, user, password);
+			
+			//conn = DriverManager.getConnection(url, user, password);
+			conn = ds.getConnection();
 			
 			//PreparedStatement 객체 생성
 			pstmt = conn.prepareStatement(sql);
@@ -84,7 +97,7 @@ public class MemberDAO {
 		String sql = "select * from testusers where id=? and pw = ?";
 		
 		try {
-			conn = DriverManager.getConnection(url,user,password);
+			conn = ds.getConnection();
 			pstmt = conn.prepareStatement(sql);
 			
 			pstmt.setString(1, id);
@@ -117,7 +130,7 @@ public class MemberDAO {
 			
 			try {
 				//객체생성
-				conn = DriverManager.getConnection(url,user,password);
+				conn = ds.getConnection();
 				//객체생성
 				pstmt = conn.prepareStatement(sql);
 				pstmt.setString(1, id);
@@ -126,6 +139,7 @@ public class MemberDAO {
 				
 				while(rs.next()) {
 					//rs.getString 을 사용해서 값을 얻어오는 코드를 작성
+					
 					String name = rs.getString("name");
 					String phone1 = rs.getString("phone1");
 					String phone2 = rs.getString("phone2");
@@ -155,7 +169,7 @@ public class MemberDAO {
 			String sql = "update testusers set pw =?, name =?, phone1 =?, phone2=?, email=?, gender=? where id =?";
 			
 			try {
-				conn = DriverManager.getConnection(url,user,password);
+				conn = ds.getConnection();
 				pstmt = conn.prepareStatement(sql);
 				
 				pstmt.setString(1, vo.getPw());
@@ -182,6 +196,34 @@ public class MemberDAO {
 			}
 			
 			return result;
+	 //회원 탈퇴 메서드 			
+		}
+		public int delete(String id) {
+			int result = 0;
+	
+			String sql = "delete from testusers where id=? ";
 			
+			try {
+				
+			conn = ds.getConnection();
+			pstmt = conn.prepareStatement(sql);
+			
+			pstmt.setString(1, id);
+			
+			result = pstmt.executeUpdate();	
+				
+			}catch(Exception e) {
+				e.printStackTrace();
+			}finally {
+				try {
+					if(conn!=null) conn.close();
+					if(pstmt!=null) pstmt.close();
+					
+				}catch(Exception e2) {
+					
+				}
+			}
+	
+			return result;
 		}
 }
--- JDBC/src/com/ok/delete_ok.java
+++ JDBC/src/com/ok/delete_ok.java
@@ -27,13 +27,16 @@ public class delete_ok extends HttpServlet {
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		//1. delete
-		//2. redirect 성공(success->login페이지)&실패(fail->mypage) 경우
-        //3. id를 통해서 탈퇴를 하고 id는 session을 이용한다.
-		//4. 세션 날리기 
+	
+		/*	1. 아이디는 세션에서 얻는다. 
+		 *  2. 메서드 생성시에 executeUpdate()메서드를 사용하여 성공시 delete(id)메서드에
+		 *  	1을 반환 : 세션을 전부 삭제후에  login.jsp
+		 *  	0을 반환 : mypage.jsp로 이동
+		 *  */
+		HttpSession session = request.getSession(); 
+		String id = (String)session.getAttribute("id"); //object 타입이기 때문에 형변환을 해줘야한다.
 		
-		HttpSession session = request.getSession();
-		String id = (String)session.getAttribute("id");
+		/*
 		String url = "jdbc:oracle:thin:@localhost:1521/XEPDB1";
 		String driver = "oracle.jdbc.driver.OracleDriver";
         String user = "myjsp";
@@ -52,21 +55,22 @@ public class delete_ok extends HttpServlet {
         	pstmt = conn.prepareStatement(sql);
         	pstmt.setString(1,id);
         	
-        	int result = pstmt.executeUpdate(); 
-       
+        	int result = pstmt.executeUpdate();
+        	 */
+           MemberDAO dao = MemberDAO.getInstance();
+      
+           int result = 0;
+           result = dao.delete(id);
         	if (result == 1) {
         		//성공시
-        		session.invalidate(); //정보를 모두 없애야 하므로 세션객체를 전부다 삭제한다.
-        	 		
-        		response.sendRedirect("login.jsp");
-        		
+        		session.invalidate(); //정보를 모두 없애야 하므로 세션객체를 전부다 삭제한다.     	 		
+        		response.sendRedirect("login.jsp");       		
         	}else {
-        		//실패시
-        	
+        		//실패시    	
         		response.sendRedirect("mypage.jsp");
         	}
         	
-        
+        /*
         }catch(ClassNotFoundException cnfe) {
         	System.out.println("드라이버 로딩실패");
         }catch(SQLException sqle){
@@ -81,12 +85,12 @@ public class delete_ok extends HttpServlet {
         		 
         	}catch(Exception e2){
         		
-        	}
+        	} */
         	
         }
 		
 	
-	}
+	
 
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response, Object PreparedStatement) throws ServletException, IOException {
